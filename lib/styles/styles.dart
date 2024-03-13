e4ca105a31466f03912e8f9a17ca4c59b1041d76
hans-4303 <103019360+hans-4303@users.noreply.github.com>
2024-03-13 09:52:54 +0900

    플러터 대시보드 시작
    flutter run으로 시작,
    isAlwaysShown은 thumbVisibility로 대체됐었음

--- lib/styles/styles.dart
+++ lib/styles/styles.dart
@@ -13,10 +13,18 @@ class Styles {
 
   static BorderRadius defaultBorderRadius = BorderRadius.circular(20);
 
+  /// isAlwaysShown 작동 안 됨.
+  /// 
+  /// https://docs.flutter.dev/release/breaking-changes/3-10-deprecations
+  /// 
+  /// 이 문서를 봤을 때 thumbVisibility로 대체되었고, 이 템플릿 상에서는 true false 값을 요구했기 때문에
+  /// 아래 같이 처리함
   static ScrollbarThemeData scrollbarTheme =
       const ScrollbarThemeData().copyWith(
     thumbColor: MaterialStateProperty.all(defaultYellowColor),
-    isAlwaysShown: false,
+    thumbVisibility: MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+      return states.contains(MaterialState.hovered) ? true : false;
+    }),
     interactive: true,
   );
 }
